14b4de8caab89678927aa1dbc168e47eefcfc91f
lichunqiang <light-li@hotmail.com>
2016-03-02 10:33:35 +0800

    initialize

---
+++ src/Skeleton.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace light\skeleton;
+
+/**
+ * Class Skeleton
+ *
+ * @version 1.0.0
+ * @author lichunqaing <light-li@hotmail.com>
+ */
+class Skeleton
+{
+    public function run()
+    {
+        return true;
+    }
+}
+
